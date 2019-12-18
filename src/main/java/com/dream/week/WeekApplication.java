34e57e3e9b606c76fc51ead716a34c720f52f576
Dream <51566157@qq.com>
2019-12-18 12:33:57 +0800

    base

---
+++ src/main/java/com/dream/week/WeekApplication.java
@@ -0,0 +1,13 @@
+package com.dream.week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dream/week/WeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream.week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
